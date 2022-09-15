270b411cba138d079566060434d01cf8e97959fc
fran810 <franjmnez9@gmail.com>
2022-09-15 15:05:00 +0100

    Codewars Challenge 15-09

---
+++ sumOfNumbers.js
@@ -0,0 +1,18 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+function getSum( a,b )
+{
+  let sum = 0;
+   //Good luck!
+  if (a === b) {
+    return a;
+  }
+  
+  else {
+    for (i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      sum += i;
+    }
+  }
+  return sum;
+}
