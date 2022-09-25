73f8b4919ca6f86bb4574752746cb2c7c8c67673
fran810 <franjmnez9@gmail.com>
2022-09-24 21:19:05 -0500

    codewars challenge practice

---
+++ removeChar.js
@@ -0,0 +1,5 @@
+function removeChar(str){
+    //Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+     return str.substring(1, str.length - 1)
+   };
+   
