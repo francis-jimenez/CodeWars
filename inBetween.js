d483d1ce738397f69dadb81bf3dfd9f8bbb91393
fran810 <franjmnez9@gmail.com>
2022-11-03 14:22:21 -0500

    codewars challenge 11-03

---
+++ inBetween.js
@@ -0,0 +1,9 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between(a, b) {
+    let numArr = []
+    for (i = a; i <= b; i++) {
+      numArr.push(i)
+    }
+    return numArr
+  }
