f1f76a737df3f2a53480d68cbae2bdfe0a7244d8
fran810 <franjmnez9@gmail.com>
2022-11-04 14:28:32 -0500

    codewars challenge 11-04

---
+++ uniqueNum.js
@@ -0,0 +1,6 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+function findUniq(arr) {
+    arr.sort((a, b) => a - b)
+    return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0]
+  }
