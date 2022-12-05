eae0e38fc081c4c0c5c795bbedc11686f5328bad
fran810 <franjmnez9@gmail.com>
2022-12-05 12:43:08 -0600

    codewars challenge 12-05

---
+++ humanReadableTime.js
@@ -0,0 +1,20 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+function humanReadable (seconds) {
+    let hours = 0;
+    let minutes = 0; 
+    while (seconds > 59)
+    if (seconds > 3599) {
+      hours++
+      seconds = seconds - 3600
+    } else {
+      minutes++
+      seconds = seconds - 60
+    }  
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  }
