63a9d94d13d3fbc34970a91e2c43dc5b21e99c19
fran810 <franjmnez9@gmail.com>
2022-12-19 17:29:00 -0600

    codewars challenge 12-19

---
+++ fillingArr.js
@@ -0,0 +1,11 @@
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+const arr = N => {
+    let newArr = [];
+    for (i = 0; i < N; i++) {
+      newArr.push(i)
+    }
+    return newArr
+  }
