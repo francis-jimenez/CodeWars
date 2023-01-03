1c5346580d502fff8cad0c9c9c1859d7b79dc2d0
fran810 <franjmnez9@gmail.com>
2023-01-03 00:54:39 -0600

    codewars challenge 1-3-23

---
+++ splitStr.js
@@ -0,0 +1,17 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+    const arr = [];
+    for (let i = 0; i < str.length; i += 2) {
+      arr.push(str.substring(i, i + 2));
+    }
+    if (str.length % 2 != 0 ) {
+      arr[arr.length - 1] += '_'
+    }
+  return arr;
+}
