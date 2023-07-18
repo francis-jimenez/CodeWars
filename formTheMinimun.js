dd60e6eae7aff471ad38845aa9f65888c88eb14f
= <franjmnez9@gmail.com>
2023-07-18 09:26:40 -0500

    codewars challenge 07-18

---
+++ formTheMinimun.js
@@ -0,0 +1,9 @@
+// Task
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+
+function minValue(values){
+    return Number([...new Set(values)].sort().join(''));
+  }
