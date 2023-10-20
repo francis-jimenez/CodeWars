f451a1f5a4f876daeebceb4f6ab0c9698b55de1a
= <franjmnez9@gmail.com>
2023-10-20 10:38:44 -0500

    basics warmup 10-20-23

---
+++ combineStr.js
@@ -0,0 +1,5 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames (fName, lName) {
+  return `${fName} ${lName}`
+}
